a3520fc745eab4fe0d390a5f311d8b2fc0a7bf7c
Alessandro Stoppato <alexrah@gmail.com>
2014-01-24 12:12:04 +0100

    back button, databases, font di stampa, menus

---
+++ plugins/content/backbutton/backbutton.php
@@ -0,0 +1,48 @@
+<?php
+// no direct access
+defined('_JEXEC') or die;
+
+jimport( 'joomla.plugin.plugin' );
+
+class plgContentBackButton extends JPlugin
+{
+	/**
+	 * Constructor
+	 *
+	 * For php4 compatability we must not use the __constructor as a constructor for plugins
+	 * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
+	 * This causes problems with cross-referencing necessary for the observer design pattern.
+	 *
+	 * @param object $subject The object to observe
+	 * @param object $params  The object that holds the plugin parameters
+	 * @since 1.5
+	 */
+	function plgContentBackbutton( &$subject, $params )
+	{
+		parent::__construct( $subject, $params );
+		$this->loadLanguage();
+	}
+	/**
+	 * Replaces {backbutton} with a back button link
+	 *
+	 * Method is called by the view
+	 *
+	 * 
+	 * @param	string	The context of the content being passed to the plugin.
+	 * @param	object	The article object.  Note $article->text is also available
+	 * @param	object	The article params
+	 * @param	int		The 'page' number
+	 *
+	 **/
+	 
+	function onContentPrepare( $context, &$article, &$params, $page=0 )
+	{
+		$app = &JFactory::getApplication();
+		$this->params->get('showindiv') == 1 ? $link = '<div class="backbutton">' : $link = '';
+		$pattern = '{backbutton}';
+		$link .= '<a href="javascript:history.back();">'. $this->params->get('linklabel', JText::_('BACK')) .'</a>';
+		$this->params->get('showindiv') == 1 ? $link .= '</div>' : $link .= '';
+		$article->text = str_replace($pattern, $link, $article->text);
+  }
+}
+?>
